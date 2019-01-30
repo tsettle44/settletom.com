a072c8cbb3e09c60516c6200641a82cf9cbe29ae
Tom Settle <tsettle44@gmail.com>
2019-01-29 21:59:41 -0500

    delete typeit add npm react-type-animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import 'semantic-ui-css/semantic.min.css'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import { Header } from 'semantic-ui-react'
-import Type from '../components/Type'
+import Typing from 'react-typing-animation'
 import About from '../components/about'
 import posed from 'react-pose'
 import wave from '../images/wave.png'
@@ -25,56 +25,60 @@ class IndexPage extends React.Component {
   }
   render() {
     return (
-      <Title pose={this.state.isVisible ? 'visible' : 'hidden'}>
-        <Layout>
-          <SEO title="Home" keywords={[`Tom Settle`, `Blog`, `Portfolio`]} />
+      // <Title pose={this.state.isVisible ? 'visible' : 'hidden'}>
+      <Layout>
+        <SEO title="Home" keywords={[`Tom Settle`, `Blog`, `Portfolio`]} />
+        <div
+          ref="canvas"
+          style={{
+            backgroundImage: 'linear-gradient(to bottom, #591995, #8c43ff)',
+            width: '100vw',
+            height: '60vh',
+            position: 'relative',
+            top: '-2px',
+          }}
+        >
           <div
-            ref="canvas"
             style={{
-              backgroundImage: 'linear-gradient(to bottom, #591995, #8c43ff)',
-              width: '100vw',
-              height: '60vh',
-              position: 'relative',
-              top: '-2px',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              textAlign: 'center',
+              height: '45vh',
             }}
           >
-            <div
+            <Header
               style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                textAlign: 'center',
-                height: '45vh',
+                color: 'white',
+                fontSize: '6rem',
               }}
+              size="huge"
             >
-              <Header
-                style={{
-                  color: 'white',
-                  fontSize: '6rem',
-                }}
-                size="huge"
-              >
-                tom settle
-                <br />
-                <span style={{ fontSize: '2rem' }}>
-                  <Type />
-                </span>
-              </Header>
-            </div>
-            <img
-              style={{
-                margin: '0',
-                width: '100vw',
-                position: 'absolute',
-                bottom: '-2px',
-              }}
-              src={wave}
-              alt="wave"
-            />
+              tom settle
+              <br />
+              <div style={{ fontSize: '2rem' }}>
+                <Typing>
+                  <span>student</span>
+                  <Typing.Delay ms={500} />
+                  <Typing.Backspace count={7} />
+                </Typing>
+              </div>
+            </Header>
           </div>
-          <About />
-        </Layout>
-      </Title>
+          <img
+            style={{
+              margin: '0',
+              width: '100vw',
+              position: 'absolute',
+              bottom: '-2px',
+            }}
+            src={wave}
+            alt="wave"
+          />
+        </div>
+        <About />
+      </Layout>
+      //   </Title>
     )
   }
 }
