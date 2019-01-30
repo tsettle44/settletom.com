4036ec42c8f825f6b662bcd787b54a77d5207b32
Tom Settle <tsettle44@gmail.com>
2019-01-29 22:35:18 -0500

    remove typing - add plugin sharp

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,20 +15,20 @@ module.exports = {
       },
     },
     `gatsby-transformer-remark`,
-    // `gatsby-transformer-sharp`,
-    // `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `Tom Settle`,
-    //     short_name: `tomsettle`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `src/images/tom.png`, // This path is relative to the root of the site.
-    //   },
-    // },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Tom Settle`,
+        short_name: `tomsettle`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/tom.png`, // This path is relative to the root of the site.
+      },
+    },
     {
       resolve: `gatsby-source-graphcms`,
       options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import 'semantic-ui-css/semantic.min.css'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import { Header } from 'semantic-ui-react'
-import Typing from 'react-typing-animation'
 import About from '../components/about'
 import posed from 'react-pose'
 import wave from '../images/wave.png'
@@ -25,60 +24,54 @@ class IndexPage extends React.Component {
   }
   render() {
     return (
-      // <Title pose={this.state.isVisible ? 'visible' : 'hidden'}>
-      <Layout>
-        <SEO title="Home" keywords={[`Tom Settle`, `Blog`, `Portfolio`]} />
-        <div
-          ref="canvas"
-          style={{
-            backgroundImage: 'linear-gradient(to bottom, #591995, #8c43ff)',
-            width: '100vw',
-            height: '60vh',
-            position: 'relative',
-            top: '-2px',
-          }}
-        >
+      <Title pose={this.state.isVisible ? 'visible' : 'hidden'}>
+        <Layout>
+          <SEO title="Home" keywords={[`Tom Settle`, `Blog`, `Portfolio`]} />
           <div
+            ref="canvas"
             style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              textAlign: 'center',
-              height: '45vh',
+              backgroundImage: 'linear-gradient(to bottom, #591995, #8c43ff)',
+              width: '100vw',
+              height: '60vh',
+              position: 'relative',
+              top: '-2px',
             }}
           >
-            <Header
+            <div
               style={{
-                color: 'white',
-                fontSize: '6rem',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                textAlign: 'center',
+                height: '45vh',
               }}
-              size="huge"
             >
-              tom settle
-              <br />
-              <div style={{ fontSize: '2rem' }}>
-                <Typing>
-                  <span>student</span>
-                  <Typing.Delay ms={500} />
-                  <Typing.Backspace count={7} />
-                </Typing>
-              </div>
-            </Header>
+              <Header
+                style={{
+                  color: 'white',
+                  fontSize: '6rem',
+                }}
+                size="huge"
+              >
+                tom settle
+                <br />
+                <div style={{ fontSize: '2rem' }} />
+              </Header>
+            </div>
+            <img
+              style={{
+                margin: '0',
+                width: '100vw',
+                position: 'absolute',
+                bottom: '-2px',
+              }}
+              src={wave}
+              alt="wave"
+            />
           </div>
-          <img
-            style={{
-              margin: '0',
-              width: '100vw',
-              position: 'absolute',
-              bottom: '-2px',
-            }}
-            src={wave}
-            alt="wave"
-          />
-        </div>
-        <About />
-      </Layout>
-      //   </Title>
+          <About />
+        </Layout>
+      </Title>
     )
   }
 }
