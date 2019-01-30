fa25788afe126a0fb4620e58e7a0b7182b46ab56
Tom Settle <tsettle44@gmail.com>
2019-01-30 13:57:05 -0500

    custom scrollbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 import Header from './header'
+import './layout.css'
 import { Container } from 'semantic-ui-react'
 
 const Layout = ({ children }) => (
