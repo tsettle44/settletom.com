5eab7e1c3fb62af2d6149be182c6058619c77964
Tom Settle <tsettle44@gmail.com>
2019-01-30 15:24:49 -0500

    finished grid

--- src/components/about.js
+++ src/components/about.js
@@ -28,7 +28,7 @@ const About = () => (
       </Grid>
     </Container>
     <div
-      style={{ width: '100vw', height: '1000px', backgroundColor: '#8c43ff' }}
+      style={{ width: '100vw', height: '700px', backgroundColor: '#8c43ff' }}
     >
       <Container>
         <Grid
@@ -39,7 +39,6 @@ const About = () => (
             borderRadius: '5px',
             position: 'relative',
             bottom: '100px',
-            height: '800px',
             backgroundColor: 'white',
             boxShadow:
               '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
@@ -54,9 +53,46 @@ const About = () => (
             textAlign="center"
           >
             <Icon color="violet" name="database" size="huge" />
-            <p style={{ marginTop: '15px', fontSize: '1.5rem' }}>
+            <p
+              style={{
+                marginTop: '15px',
+                fontSize: '1.5rem',
+                fontWeight: 'bold',
+              }}
+            >
               Backend Development
             </p>
+            <p style={{ marginTop: '25px', fontSize: '1rem' }}>
+              Secure RESTful API's built to receive, send, update, and delete
+              data that can be integrated with a client-side website and a
+              database to securely save your data.
+            </p>
+            <p
+              style={{
+                marginTop: '25px',
+                fontSize: '1.25rem',
+                color: '#8c43ff',
+              }}
+            >
+              Languages/Tech
+            </p>
+            <p>Nodejs</p>
+            <p>Expressjs</p>
+            <p>MongoDB</p>
+            <p>GraphQL</p>
+            <p
+              style={{
+                marginTop: '25px',
+                fontSize: '1.25rem',
+                color: '#8c43ff',
+              }}
+            >
+              Tools
+            </p>
+            <p>GraphCMS (Headless CMS)</p>
+            <p>Mlab</p>
+            <p>OAuth</p>
+            <p>Postman</p>
           </Grid.Column>
           <Grid.Column
             style={{
@@ -67,13 +103,92 @@ const About = () => (
             textAlign="center"
           >
             <Icon color="violet" name="computer" size="huge" />
-            <p style={{ marginTop: '15px', fontSize: '1.5rem' }}>
+            <p
+              style={{
+                marginTop: '15px',
+                fontSize: '1.5rem',
+                fontWeight: 'bold',
+              }}
+            >
               Frontend Development
             </p>
+            <p style={{ marginTop: '25px', fontSize: '1rem' }}>
+              I am a framework/library lover. Currently obsessed with React and
+              Gatsby
+              <br />
+              <br />
+              <br />
+            </p>
+            <p
+              style={{
+                marginTop: '25px',
+                fontSize: '1.25rem',
+                color: '#8c43ff',
+              }}
+            >
+              Languages/Tech
+            </p>
+            <p>React</p>
+            <p>HTML</p>
+            <p>CSS</p>
+            <p>Vanila JavaScript</p>
+            <p
+              style={{
+                marginTop: '25px',
+                fontSize: '1.25rem',
+                color: '#8c43ff',
+              }}
+            >
+              Tools
+            </p>
+            <p>VSCode</p>
+            <p>Git</p>
+            <p>Hyper</p>
+            <p>Bootstrap</p>
+            <p>Gatsby</p>
           </Grid.Column>
           <Grid.Column style={{ paddingTop: '20px' }} textAlign="center">
             <Icon color="violet" name="paint brush" size="huge" />
-            <p style={{ marginTop: '15px', fontSize: '1.5rem' }}>Design</p>
+            <p
+              style={{
+                marginTop: '15px',
+                fontSize: '1.5rem',
+                fontWeight: 'bold',
+              }}
+            >
+              Design
+            </p>
+            <p style={{ marginTop: '25px', fontSize: '1rem' }}>
+              I love to dabble in design and keep my mind creative. I have a
+              great appreciaiton for simple website/app design and bright colors
+              <br />
+              <br />
+            </p>
+            <p
+              style={{
+                marginTop: '25px',
+                fontSize: '1.25rem',
+                color: '#8c43ff',
+              }}
+            >
+              Design Specialties
+            </p>
+            <p>UI/UX</p>
+            <p>Web</p>
+            <p>iOS/Android</p>
+            <p>Logos</p>
+            <p
+              style={{
+                marginTop: '25px',
+                fontSize: '1.25rem',
+                color: '#8c43ff',
+              }}
+            >
+              Tools
+            </p>
+            <p>Adobe Illustrator</p>
+            <p>Adobe Photoshop</p>
+            <p>Adobe Xd</p>
           </Grid.Column>
         </Grid>
       </Container>
