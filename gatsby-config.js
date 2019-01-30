52a44db8f9254e29ddd4e6bcd2713b93dc828927
Tom Settle <tsettle44@gmail.com>
2019-01-29 22:59:38 -0500

    deploy for pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,7 @@ module.exports = {
         icon: `src/images/tom.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-offline`,
     {
       resolve: `gatsby-source-graphcms`,
       options: {
