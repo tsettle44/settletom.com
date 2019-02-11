7779639f3834a324268ad3494c5dd9cd819b1393
Tom Settle <tsettle44@gmail.com>
2019-02-11 11:29:07 -0500

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,22 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-134285442-1',
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: 'settletom.com',
+      },
+    },
     `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
