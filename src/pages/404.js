006dfde049f7c8e7adba5bd867462b4441b68f52
Tom Settle <tsettle44@gmail.com>
2019-02-15 15:56:31 -0500

    add link to 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import 'semantic-ui-css/semantic.min.css'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
@@ -9,6 +10,11 @@ const NotFoundPage = () => (
     <div style={{ height: '100vh', textAlign: 'center', marginTop: '100px' }}>
       <h1>NOT FOUND</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <Link to="/">
+        <p style={{ color: '#8c43ff', textDecoration: 'underline' }}>
+          Take me home
+        </p>
+      </Link>
     </div>
   </Layout>
 )
