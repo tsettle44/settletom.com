a0c73911f4d6f0bd62ce02394a6d558c8346811f
Tom Settle <tsettle44@gmail.com>
2020-05-18 13:31:19 -0600

    add photo galleries skeloton

---
+++ src/pages/photos.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import 'semantic-ui-css/semantic.min.css'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const Photos = () => (
+  <Layout>
+    <SEO title="Photo Galleries" />
+    <h1>Photos</h1>
+  </Layout>
+)
+
+export default Photos
